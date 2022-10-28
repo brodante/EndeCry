8f19ffbb6de35c5a18ba62e5754a568e2110f7ff
Surya <44796607+brodante@users.noreply.github.com>
2022-10-29 00:21:53 +0530

    Add files via upload
    all files are uploaded independently, from my "project endecry" folder

---
+++ project endecry/project 2nd yr/proj-testing.cpp
@@ -0,0 +1,30 @@
+/*Code by Surya a.k.a Sunny*/
+/* by https://www.codechef.com/users/spsc */
+#include <bits/stdc++.h>
+//#include <graphics.h>
+#include <conio.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+#define lli long long
+#define pi 3.14159265358979323846
+#define MOD 1000000007
+#define unbuffer cin.clear(); cin.sync();
+#define foi(n)  for(lli i=0;i<n;i++)
+#define foj(n)  for(lli j=0;j<n;j++)
+#define test(T) lli T;cin>>T;while(T--)
+#define loop(i, a, b) for(int i = (a); i<= (b); i++)
+using namespace std;
+//using namespace boost::multiprecision;
+int main()
+{
+  ios_base::sync_with_stdio(false);
+   //cin.tie(NULL);
+   //cout.tie(NULL);
+
+   string a;
+   cin>>a;
+   ofstream file;
+   file.open(a);
+   cout<<a;
+   file<<"Hello world";
+   file.close();
+ }
